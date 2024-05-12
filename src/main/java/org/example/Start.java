9d06b7f4cbdf37204ef682686be55fa3ddd58255
3amo-magdy <amoramagdymahmoud@gmail.com>
2024-05-12 03:49:55 +0300

    added some comments + server thread creation and signaling.

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -4,9 +4,29 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import org.example.server.ServerT;
+import org.example.server.ServerThread;
 
 public class Start {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // a signaling mechanism to get notified when the server thread is ready to recieve requests
+        CountDownLatch latch = new CountDownLatch(1);
+        
+        // Create the server thread
+        Thread serverThread = new ServerThread(latch);
+        
+        // Start the server thread (it should start reading the graph)
+        serverThread.start();
+        
+        latch.await(); // Wait for server's signal
 
+        System.out.println("Received signal from the server thread, starting clients...");
+        
+        // Start the client processes
+        // ...
+
+        serverThread.join(); // Or detach?
     }
 }
---
+++ src/main/java/org/example/server/ServerThread.java
@@ -0,0 +1,51 @@
+package org.example.server;
+
+import org.example.utils.GetPropValues;
+
+import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.util.Properties;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ServerThread extends Thread {
+    private CountDownLatch latch;   // To signal the parent thread (Start.java:main) when ready to handle requests.
+    private int port = 49053;
+
+    public ServerThread(CountDownLatch latch) {
+        this.latch = latch;
+        
+        try {
+            // Get Rmi Registery Server and port from system.properties
+            Properties props = GetPropValues.getPropValues();
+            port = Integer.parseInt(props.getProperty("GSP.rmiregistry.port"));
+            
+            // Create Object of the implementation of remote interface
+            GSPRemoteInterface obj = new GSPRemoteObject();
+
+            // Bind the remote object with the name //rmi:://localhost:port/gsp
+            LocateRegistry.createRegistry(port);
+            Naming.rebind(GetPropValues.getRemoteObjectReference(), obj);
+        
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
+
+    @Override
+    public void run() {
+        // Read Graph from standard input.
+        // ...
+
+        // Signal the parent thread when ready.
+        latch.countDown();
+
+        // Accept requests.
+        // ...
+    }
+}
